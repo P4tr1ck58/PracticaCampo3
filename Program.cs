5ff6fb6a32536415a88569510e014023883e2157
Patrick <n00488859@upn.pe>
2025-04-14 22:33:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaCampo3_PatrickMallqui
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
